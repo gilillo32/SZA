4c6f85f42dccd0a11d2d58b9acef107f8332b45b
Mikel Laorden <laordenmikel@gmail.com>
2021-10-24 01:21:37 +0200

    NOWGETkomadoa metodoa 2 metodo nagusietan zatitu + erroreak exception-en bitartez maneiatu
    NOWGETkomadoa metodoa 4 metodo-etan zatitua izan da, hau da, NOWGETkomadoa eta beste 3 metodo pribatu gehiago:
    - __NOWGETkomandoaParamEz(aukera)
    - __NOWGETkomandoaParamBai(aukera, parametroak)
    - __parseTenperatura(tenperatura) : tenperatura protokoloak zehazten duen string formatu egokian bihurtzen du.
    
    Beste aldetik erroreen tratamendua, salbuespenen (Exception) bitartez burutu da. Orain errore bat dagoenean, metodoek ErroreaEskaeran motako salbuespen bat jaurtiko dute. Salbuespen hau heredatzen duten hurrengo salbuespen zehatzagoak sortu dira, errore kode zehatz batekin sortzen direnak alegia:
    - ErrKomandoEzezaguna: erroreKodea = 1 du default eran
    - ErrEzEsperoParam: erroreKodea = 2 du default eran
    - ErrHautaParamFalta: erroreKodea = 3 du default eran
    - ErrParamFormatuEzEgoki: erroreKodea = 4 du default eran
    Aurreko 4 klaseen eraikitzailea mugatua izago da eta ezin izango da adibidez ErrKomandoEzezaguna instantzia bat 12 errore kodearekin hasieratu. Aurreko klaseek errore kode finko bat izango dute beti.
    
    ErroreaEskaeran klaseak, objektu baten errore kodea lortzeko "get_errore_kode()" metodoa du. ErroreaEskaeran objaktu baten errore kodea, objektua eraikitzeko unean bakarrik aldatu daiteke. ErroreaEskaeran objektuaren instanzia baten errore kodea aldatu nahi bada, orduan instantzia berria sortu beharko da bertako eraikitzailean nahi den kodea zehaztuz.
    
    ErroreaEskaeran klase guztietan errorearen deskribapen bat gehitu ahal izango da. Deskribapen hau hautazkoa izango da. Hau objektua string-era pasatzeko unean erabilgarria izan daiteke, bertan errorearen kodearekin batera honen deskribapena ere agertuko baita.

--- erradiadore_zer_udp.py
+++ erradiadore_zer_udp.py
@@ -1,101 +1,107 @@
 #!/usr/bin/env python3
 import socket
 import BerogailuLista
+from zerbitzari_errore import ErroreaEskaeran, ErrParamFormatuEzEgoki, ErrEzEsperoParam, ErrHautaParamFalta, \
+    ErrKomandoEzezaguna
 
 PORT = 50001
 MAX_BYTES_DATAGRAM = 1500
 
 berogailuak = BerogailuLista.BerogailuLista()
 
-
-def NOWGETkomandoa(aukera):
+def NOWGETkomandoa(aukera, parametroak):
     """
     NOW eta GET komandoekin egiten dena berdina da, baina batek uneko tenperaturarekin
     egiten du eta besteak desio tenperaturarekin. Beraz, metodo orokor bat egingo dugu
     zeinak sartzen duzun aukeraren(NOW edo GET) arabera desio edo uneko tenperatura
     itzultzen duen.
+    :param parametroak:
     """
     if not parametroak:
-        # berogailu bakoitzean dagoen uneko hozberoa
-        iterator = berogailuak.getIteradorea()
-        erantzunaren_parte = ""
-        for ber in iterator:
+        ema = __NOWGETkomandoaParamEz(aukera)
+    else:  # parametroa sartu da
+        ema = __NOWGETkomandoaParamBai(aukera, parametroak)
+    return __zerrendaLuzeeraMaxEzGainditu(ema)
+
+
+def __NOWGETkomandoaParamEz(aukera):
+    # berogailu bakoitzean dagoen uneko hozberoa
+    iterator = berogailuak.getIteradorea()
+    erantzunaren_parte = ""
+    for ber in iterator:
+        tenperatura = 0
+        if aukera == "NOW":  # uneko hozberoa nahi dugu
+            tenperatura = ber.unekoHozberoaBueltatu()
+        elif aukera == "GET":  # desio hozberoa nahi dugu
+            tenperatura = ber.desioHozberoaBueltatu()
+
+        erantzunaren_parte += __parseTenperatura(tenperatura) + ":"
+    ema = ("+" + erantzunaren_parte[
+                 0:len(erantzunaren_parte) - 1])  # ez dugu bueltatu behar azkenengo karakterea (:)
+    return ema
+
+
+def __NOWGETkomandoaParamBai(aukera, parametroak):
+    try:
+        # parametroa id-a dago
+        berogailua = berogailuak.bilatuId(int(parametroak))
+        if berogailua is None:  # id hori duen berogailurik ez da existitzen
+            if aukera == "NOW":
+                raise ErroreaEskaeran(14)  # errorea 14 izango da
+            elif aukera == "GET":
+                raise ErroreaEskaeran(15)  # errorea 15 izango da
+
+        else:  # existitzen da
             tenperatura = 0
             if aukera == "NOW":  # uneko hozberoa nahi dugu
-                tenperatura = ber.unekoHozberoaBueltatu()
+                tenperatura = berogailua.unekoHozberoaBueltatu()
             elif aukera == "GET":  # desio hozberoa nahi dugu
-                tenperatura = ber.desioHozberoaBueltatu()
+                tenperatura = berogailua.desioHozberoaBueltatu()
 
-            # bakarrik nahi dugu atal osoa eta gainera 3 zifrako zenbakia izango da koma kenduta eta tenperatua<10
-            # bada 0 gehitu aurrean 3 zenbaki atal osoan lortu
-            tenperatura *= 10
-            # atal dezimala lortu
-            tenperatura = int(tenperatura)
-            # ikusi ea 3 zifrako zenbakia den, bestela 0 gehitu aurrean
-            if len(str(tenperatura)) == 2:  # 0 gehitu ezkerrean
-                erantzunaren_parte += "0" + str(int(tenperatura)) + ":"
-            elif len(str(tenperatura)) == 1:  # 00 gehitu ezkerrean
-                erantzunaren_parte += "00" + str(int(tenperatura)) + ":"
-            else:
-                erantzunaren_parte += str(int(tenperatura)) + ":"
+            ema = "+" + __parseTenperatura(tenperatura)
 
-        ema = ("+" + erantzunaren_parte[
-                     0:len(erantzunaren_parte) - 1])  # ez dugu bueltatu behar azkenengo karakterea (:)
-    else:  # parametroa sartu da
-        try:
-            # parametroa id-a dago
-            berogailua = berogailuak.bilatuId(int(parametroak))
-            if berogailua is None:  # id hori duen berogailurik ez da existitzen
-                if aukera == "NOW":
-                    ema = '-14'  # errorea 14 izango da
-                elif aukera == "GET":
-                    ema = '-15'  # errorea 15 izango da
+    except ValueError:
+        # kasting-a ezin bada egin string bat delako--> parametroak ez du forma egokia
+        raise ErrParamFormatuEzEgoki()  # errore 4 itzuli
+    return ema
 
-            else:  # existitzen da
-                tenperatura = 0
-                if aukera == "NOW":  # uneko hozberoa nahi dugu
-                    tenperatura = berogailua.unekoHozberoaBueltatu()
-                elif aukera == "GET":  # desio hozberoa nahi dugu
-                    tenperatura = berogailua.desioHozberoaBueltatu()
 
-                # bakarrik nahi dugu atal osoa eta gainera 3 zifrako zenbakia izango da koma kenduta eta
-                # tenperatua<10 bada 0 gehitu aurrean 3 zenbaki atal osoan lortu
-                tenperatura *= 10
-                # atal dezimala lortu
-                tenperatura = int(tenperatura)
-                # ikusi ea 3 zifrako zenbakia den, bestela 0 gehitu aurrean
-                if len(str(tenperatura)) == 2:  # 0 gehitu ezkerrean
-                    tenperatura = "0" + str(int(tenperatura))
-                elif len(str(tenperatura)) == 1:  # 00 gehitu ezkerrean
-                    tenperatura = "00" + str(int(tenperatura))
-                ema = ("+" + str(tenperatura))
-
-        except ValueError:
-            # kasting-a ezin bada egin string bat delako--> parametroak ez du forma egokia
-            ema = "-4"  # errore 4 itzuli
+def __parseTenperatura(tenperatura):
+    # bakarrik nahi dugu atal osoa eta gainera 3 zifrako zenbakia izango da koma kenduta eta
+    # tenperatua<10 bada 0 gehitu aurrean 3 zenbaki atal osoan lortu
+    tenperatura *= 10
+    # atal dezimala lortu
+    tenperatura = int(tenperatura)
+    # ikusi ea 3 zifrako zenbakia den, bestela 0 gehitu aurrean
+    if len(str(tenperatura)) == 2:  # 0 gehitu ezkerrean
+        tenperatura = "0" + str(tenperatura)
+    elif len(str(tenperatura)) == 1:  # 00 gehitu ezkerrean
+        tenperatura = "00" + str(tenperatura)
+    ema = str(tenperatura)
     return ema
 
 
 def OFFkomandoa(id_berogailu):
-    bueltan = '+'
-    if not id_berogailu:
-        for bg in berogailuak.getIteradorea():
-            if bg.getEgoera():
-                bg.egoeraAldatu(False)
-    else:
-        berogailua = berogailuak.bilatuId(int(id_berogailu))
-        if berogailua is None:
-            bueltan = '-12'
-        else:
-            berogailua.egoeraAldatu(False)
+    try:
+        bueltan = __berogailuEgoeraAldatu(id_berogailu, False)
+    except ErroreaEskaeran:
+        raise ErroreaEskaeran(12) # Err OFF. Ez dago ID hori duen berogailurik
+
     return bueltan
 
 
 def ONNkomandoa(id_berogailu):
-    errorekodea = 0  # 0 bada ez dago errorerik oraindik
+    try:
+        bueltan = __berogailuEgoeraAldatu(id_berogailu, True)
+    except ErroreaEskaeran:
+        raise ErroreaEskaeran(11) # Err ONN. Ez dago ID hori duen berogailurik
+
+    return bueltan
+
+def __berogailuEgoeraAldatu(id_berogailu, egoera):
     if not id_berogailu:
         # Berogailu guztiak piztu: ez da parametrorik jaso
-        berogailuak.egoeraAldatuGuztiei(True)
+        berogailuak.egoeraAldatuGuztiei(egoera)
     else:
         # Frogaketak:
         try:  # Jaso den parametroa zenbaki bat den frogatu (ID bat izango da eta)
@@ -103,27 +109,22 @@ def ONNkomandoa(id_berogailu):
             if id_zenb < 0:  # Jasotako zenbakia ez da negatiboa
                 raise ValueError()
         except ValueError:
-            errorekodea = 4  # Formatu errorea: Jasotako parametroa ez da zenbaki bat edo zenbaki negatiboa da
+            raise ErrParamFormatuEzEgoki()  # Formatu errorea: Jasotako parametroa ez da zenbaki bat edo zenbaki negatiboa da
 
-        if not errorekodea:
-            unek = berogailuak.bilatuId(id_zenb)
-            if unek is not None:
-                # ID- hori duen berogailua piztu
-                unek.egoeraAldatu(True)
-            else:
-                # Ezin da eragiketa burutu
-                errorekodea = 11  # Err ONN. Ez dago '{id_zenb}' ID-a duen berogailurik
+        unek = berogailuak.bilatuId(id_zenb)
+        if unek is not None:
+            # ID- hori duen berogailuari egoera aldatu
+            unek.egoeraAldatu(egoera)
+        else:
+            # Ezin da eragiketa burutu
+            raise ErroreaEskaeran()
 
-    if not errorekodea:
-        bueltan = "+"
-    else:
-        bueltan = "-" + str(errorekodea)
+    bueltan = "+"
     return bueltan
 
 
-def NAMkomandoa(parametroak):
-    errorekodea = 0 # 0 bada ez dago errorerik oraindik
 
+def NAMkomandoa(parametroak):
     # Berogailuen deskribapenak ("ID,izena" tuplak) gordeko diren array-a hasieratu
     berogdeskriblista = []
     # Berogailu bakoitza iteratzeko, berogailu listari iteradore bat eskatu eta aldagai batean gorde
@@ -142,20 +143,16 @@ def NAMkomandoa(parametroak):
             bueltan = ":".join(berogdeskriblista)
         except Exception:
             # Datuen bilketan edo prozezaketan errore bat egon bada, orduan egoera ez egokia dela adierazi
-            errorekodea = 13
+            raise ErroreaEskaeran(13)
     else:
-        errorekodea = 2 # NAM komandoak parametroak jaso ditu eta ez luke parametrorik jaso beharko.
+        raise ErrEzEsperoParam() # NAM komandoak parametroak jaso ditu eta ez luke parametrorik jaso beharko.
 
-    if not errorekodea:
-        # Bidaliko den mezua prestatu
-        bueltan = "+" + bueltan
 
-        # Bidaliko den mezua luzeera maximoa gainditzen ez duela frogatu eta gainditzen badu murriztu
-        bueltan = __zerrendaLuzeeraMaxEzGainditu(bueltan)
+    bueltan = "+" + bueltan
+    # Bidaliko den mezua luzeera maximoa gainditzen ez duela frogatu eta gainditzen badu murriztu
+    bueltan = __zerrendaLuzeeraMaxEzGainditu(bueltan)
 
-    else:
-        # Egoera ez egokia: Errorea mezuaren exekuzioan gertatu da, errore mezua prestatu
-        bueltan = "-" + str(errorekodea)
+
     return bueltan
 
 def __zerrendaLuzeeraMaxEzGainditu(zerrenda):
@@ -177,7 +174,7 @@ def __zerrendaLuzeeraMaxEzGainditu(zerrenda):
 
 def SETkomandoa(param):
     if not param:
-        return '-3'
+        raise ErrHautaParamFalta()
     else:
         tenp = param[:3]
         bg_id = param[3:]
@@ -187,53 +184,60 @@ def SETkomandoa(param):
             if tenp < 0:
                 raise ValueError()
         except ValueError:  # Hozberoa zenbaki osoa izan behar da
-            return '-4'
+            raise ErrParamFormatuEzEgoki()
         if not bg_id:
             for bg in berogailuak.getIteradorea():
                 bg.setDesioTenp(tenp)
         else:
             bg = berogailuak.bilatuId(bg_id)
             if not bg:
-                return '-16'
+                raise ErroreaEskaeran(16)
             bg.setDesioTenp(tenp)
         if not egoeraEgokia:
-            return '-16'
+            raise ErroreaEskaeran(16)
 
 
+def main():
+    # Sortu socketa eta esleitu helbide bat.
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(('', PORT))
 
-# Sortu socketa eta esleitu helbide bat.
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('', PORT))
+    while True:
+        # Jaso mezu bat eta erantzun datu berdinekin.
+        mezua, bez_helb = s.recvfrom(1024)
+        # Jasotako mezua ASCII formatuan dekodetu
+        mezua = mezua.decode("ascii")
+        # komandoak 3 parametro izango dituzte
+        komandoa = mezua[:3]
+        # parametroak (izatekotan) komandoen atzetik doaz
+        parametroak = mezua[3:]
+        # Erantzuna hasieratu
+        erantzuna = ''
+        try:
+            # sartutako komando bakoitzeko kasu bat
+            if komandoa == "ONN":
+                erantzuna = ONNkomandoa(parametroak).encode("ascii")
+            elif komandoa == "OFF":
+                erantzuna = OFFkomandoa(parametroak).encode("ascii")
+            elif komandoa == "NAM":
+                erantzuna = NAMkomandoa(parametroak).encode("utf-8")
+            elif komandoa == "NOW":
+                erantzuna = NOWGETkomandoa("NOW", parametroak).encode("ascii")
+            elif komandoa == "GET":
+                erantzuna = NOWGETkomandoa("GET", parametroak).encode("ascii")
+            elif komandoa == "SET":
+                erantzuna = SETkomandoa(parametroak).encode("ascii")
+            else:
+                raise ErrKomandoEzezaguna() # komando ezezaguna
+        except ErroreaEskaeran as errEsk:
+            erantzuna = "-" + errEsk.get_errore_kode()
+            erantzuna = erantzuna.encode("ascii")
 
-while True:
-    # Jaso mezu bat eta erantzun datu berdinekin.
-    mezua, bez_helb = s.recvfrom(1024)
-    # Jasotako mezua ASCII formatuan dekodetu
-    mezua = mezua.decode("ascii")
-    # komandoak 3 parametro izango dituzte
-    komandoa = mezua[:3]
-    # parametroak (izatekotan) komandoen atzetik doaz
-    parametroak = mezua[3:]
-    # Erantzuna hasieratu
-    erantzuna = ''
+        s.sendto(erantzuna, bez_helb)
+    # noinspection PyUnreachableCode
+    s.close()
 
-    # sartutako komando bakoitzeko kasu bat
-    if komandoa == "ONN":
-        erantzuna = ONNkomandoa(parametroak).encode("ascii")
-    elif komandoa == "OFF":
-        erantzuna = OFFkomandoa(parametroak).encode("ascii")
-    elif komandoa == "NAM":
-        erantzuna = NAMkomandoa(parametroak).encode("utf-8")
-    elif komandoa == "NOW":
-        erantzuna = NOWGETkomandoa("NOW").encode("ascii")
-    elif komandoa == "GET":
-        erantzuna = NOWGETkomandoa("GET").encode("ascii")
-    elif komandoa == "SET":
-        erantzuna = SETkomandoa(parametroak).encode("ascii")
-    else:
-        # komando ezezaguna
-        erantzuna = "-1".encode("ascii")  # errorea
-
-    s.sendto(erantzuna, bez_helb)
-# noinspection PyUnreachableCode
-s.close()
+# Programa hau explizituki exekutatzen bada, hau da, ez da inportatzen adibidez:
+if __name__ == '__main__':
+    # Programa nagusia exekutatu
+    main()
---
+++ zerbitzari_errore.py
@@ -0,0 +1,43 @@
+from typing import Final
+
+class ErroreaEskaeran(Exception):
+    def __init__(self, errorekodea=0, erroredeskrib=''):
+        self.errore_kodea = errorekodea
+        self.errore_mezua = erroredeskrib
+
+    def get_errore_kode(self):
+        return self.errore_kodea
+
+    def __repr__(self):
+        mezua = "Err " + self.get_errore_kode()
+        if self.errore_mezua:
+            mezua += ": " + self.errore_mezua
+        return mezua
+
+class ErrKomandoEzezaguna(ErroreaEskaeran):
+    OINARRIZKO_ERR_KODE: Final[int] = 1
+
+    def __init__(self, erroredeskrib=''):
+        super().__init__(self.OINARRIZKO_ERR_KODE, erroredeskrib)
+
+
+class ErrEzEsperoParam(ErroreaEskaeran):
+    OINARRIZKO_ERR_KODE: Final[int] = 2
+
+    def __init__(self, erroredeskrib=''):
+        super().__init__(self.OINARRIZKO_ERR_KODE, erroredeskrib)
+
+
+class ErrHautaParamFalta(ErroreaEskaeran):
+    OINARRIZKO_ERR_KODE: Final[int] = 3
+
+    def __init__(self, erroredeskrib=''):
+        super().__init__(self.OINARRIZKO_ERR_KODE, erroredeskrib)
+
+
+class ErrParamFormatuEzEgoki(ErroreaEskaeran):
+    OINARRIZKO_ERR_KODE: Final[int] = 4
+
+    def __init__(self, erroredeskrib=''):
+        super().__init__(self.OINARRIZKO_ERR_KODE, erroredeskrib)
+
